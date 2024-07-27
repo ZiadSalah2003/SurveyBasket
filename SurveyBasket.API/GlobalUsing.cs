82405baec9f04a895560b658247d3bdb539ae95c
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-07-28 02:30:50 +0300

    Update Global using

--- SurveyBasket.API/GlobalUsing.cs
+++ SurveyBasket.API/GlobalUsing.cs
@@ -1,3 +1,11 @@
 ﻿global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 global using SurveyBasket.API.Entities;
+global using SurveyBasket.API.Abstractions;
+global using Microsoft.EntityFrameworkCore;
+global using SurveyBasket.API.Errors;
+global using SurveyBasket.API.Persistence;
+global using Microsoft.AspNetCore.Identity;
+global using SurveyBasket.API.Authentication;
+global using SurveyBasket.API.Contracts.cs.Authentication;
+global using System.Security.Cryptography;
