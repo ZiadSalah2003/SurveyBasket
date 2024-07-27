4dae75eb1eeb83e045dc203196a4d0bd5064ff4b
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-07-28 02:31:22 +0300

    Add Erro Class For Auth & Poll Service

---
+++ SurveyBasket.API/Errors/PollErrors.cs
@@ -0,0 +1,7 @@
+﻿namespace SurveyBasket.API.Errors
+{
+	public static class PollErrors
+	{
+		public static readonly Error PollNotFound = new("Poll.NotFound", "No poll was found with the given id");
+	}
+}
