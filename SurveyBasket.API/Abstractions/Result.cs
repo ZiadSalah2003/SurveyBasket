15d50ba47d85209f293feae98a8af907a489e852
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-07-28 02:30:17 +0300

    Adding Result & Error Class

---
+++ SurveyBasket.API/Abstractions/Result.cs
@@ -0,0 +1,31 @@
+﻿using SurveyBasket.API.Abstractions;
+
+namespace SurveyBasket.API.Abstractions
+{
+	public class Result
+	{
+		public bool IsSuccess { get; }
+		public bool IsFailure => !IsSuccess;
+		public Error Error { get; } = default!;
+		public Result(bool isSuccess, Error error)
+		{
+			if ((isSuccess && error != Error.None) || (!isSuccess && error == Error.None))
+				throw new InvalidOperationException();
+			IsSuccess = isSuccess;
+			Error = error;
+		}
+		public static Result Success() => new(true, Error.None);
+		public static Result Failure(Error error) => new(false, error);
+		public static Result<T> Success<T>(T value) => new(value, true, Error.None);
+		public static Result<T> Failure<T>(Error error) => new(default, false, error);
+	}
+}
+public class Result<T> : Result
+{
+	private readonly T? _value;
+	public Result(T? value, bool isSuccess, Error error) : base(isSuccess, error)
+	{
+		_value = value;
+	}
+	public T Value => IsSuccess ? _value! : throw new InvalidOperationException("Failure results Cannot have value");
+}
