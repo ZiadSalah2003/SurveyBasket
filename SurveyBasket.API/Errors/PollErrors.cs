7e44f9ce96c9db7a870b40f61a3e59274def194f
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-07-31 21:57:39 +0300

    adding Duplicatedpoll error

--- SurveyBasket.API/Errors/PollErrors.cs
+++ SurveyBasket.API/Errors/PollErrors.cs
@@ -3,5 +3,6 @@
 	public static class PollErrors
 	{
 		public static readonly Error PollNotFound = new("Poll.NotFound", "No poll was found with the given id");
+		public static readonly Error DuplicatedPollTitle = new("Poll.DuplicatedTitle", "Poll with the same title is already exists");
 	}
 }
