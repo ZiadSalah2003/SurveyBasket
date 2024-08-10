68657fec2a95afbd8a5d7ec357dc3efdb863ed62
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-11 02:56:40 +0300

    Adding Answer and Question Property

--- SurveyBasket.API/Entities/Poll.cs
+++ SurveyBasket.API/Entities/Poll.cs
@@ -8,5 +8,6 @@
 		public bool IsPublished { get; set; }
 		public DateOnly StartsAt { get; set; }
 		public DateOnly EndsAt { get; set; }
+		public ICollection<Question> Questions { get; set; } = [];
 	}
 }
