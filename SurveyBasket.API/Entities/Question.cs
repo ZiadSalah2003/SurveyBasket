a4ce47b8bde9b152b7288b4dcd38cc4495f63fda
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-11 02:56:13 +0300

    Add Question Entity and there configuratuion

---
+++ SurveyBasket.API/Entities/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace SurveyBasket.API.Entities
+{
+	public sealed class Question : AuditableEntity
+	{
+		public int Id { get; set; }
+		public string Content { get; set; } = string.Empty;
+		public int PollId { get; set; }
+		public bool IsActive { get; set; } = true;
+		public Poll Poll { get; set; } = default!;
+		public ICollection<Answer> Answers { get; set; } = [];
+	}
+}
---
+++ SurveyBasket.API/Persistence/EntitiesConfigrations/QuestionConfigration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SurveyBasket.API.Persistence.EntitiesConfigrations
+{
+	public class QuestionConfigration : IEntityTypeConfiguration<Question>
+	{
+		public void Configure(EntityTypeBuilder<Question> builder)
+		{
+			builder.HasIndex(x => new {x.PollId,x.Content }).IsUnique();
+			builder.Property(x=>x.Content).HasMaxLength(1000);			 
+		}
+	}
+}
