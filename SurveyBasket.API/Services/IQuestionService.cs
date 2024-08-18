c6a79f2fce493733d3546ba67cf3436a5040a73f
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-18 18:33:16 +0300

    add get avaliable method and its implementation

--- SurveyBasket.API/Services/IQuestionService.cs
+++ SurveyBasket.API/Services/IQuestionService.cs
@@ -5,6 +5,7 @@ namespace SurveyBasket.API.Services
 	public interface IQuestionService
 	{
 		Task<Result<IEnumerable<QuestionResponse>>> GetAllAsync(int pollId, CancellationToken cancellationToken = default);
+		Task<Result<IEnumerable<QuestionResponse>>> GetAvaliableAsync(int pollId, string userId, CancellationToken cancellationToken = default);
 		Task<Result<QuestionResponse>> GetAsync(int pollId, int id, CancellationToken cancellationToken = default);
 		Task<Result<QuestionResponse>> AddAsync(int pollId, QuestionRequest question, CancellationToken cancellationToken = default);
 		Task<Result> UpdateAsync(int pollId, int id, QuestionRequest request, CancellationToken cancellationToken = default);
---
+++ SurveyBasket.API/Services/IVoteService.cs
@@ -0,0 +1,9 @@
+﻿using SurveyBasket.API.Contracts.cs.Votes;
+
+namespace SurveyBasket.API.Services
+{
+	public interface IVoteService
+	{
+		Task<Result> AddAsync(int pollId, string userId, VoteRequest request, CancellationToken cancellationToken = default);
+	}
+}
