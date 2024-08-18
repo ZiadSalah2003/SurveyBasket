3d5419611fcf05edba290f84ab1e84ddd4e7504d
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-18 18:33:42 +0300

    add start and vote endpoint for vote entity

---
+++ SurveyBasket.API/Controllers/VotesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SurveyBasket.API.Contracts.cs.Votes;
+using SurveyBasket.API.Extensions;
+using SurveyBasket.API.Services;
+using System.Security.Claims;
+
+namespace SurveyBasket.API.Controllers
+{
+	[Route("api/polls/{pollId}/vote")]
+	[ApiController]
+	public class VotesController(IQuestionService questionService, IVoteService voteService) : ControllerBase
+	{
+		private readonly IQuestionService _questionService = questionService;
+		private readonly IVoteService _voteService = voteService;
+		[HttpGet]
+		public async Task<IActionResult> Start([FromRoute] int pollId, CancellationToken cancellationToken)
+		{
+			var userId = User.GetUserId();
+			var result = await _questionService.GetAvaliableAsync(pollId, userId!, cancellationToken);
+
+			return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+		}
+
+		[HttpPost("")]
+		public async Task<IActionResult> Vote([FromRoute] int pollId, [FromBody] VoteRequest request, CancellationToken cancellationToken)
+		{
+			var result = await _voteService.AddAsync(pollId, User.GetUserId()!, request, cancellationToken);
+			return result.IsSuccess ? Created() : result.ToProblem();
+		}
+	}
+}
