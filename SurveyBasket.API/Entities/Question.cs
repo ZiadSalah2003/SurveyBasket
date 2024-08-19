cd8b761502ff59081027c57388732564a2559440
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-19 22:35:22 +0300

    add collection of vote answer in question table

--- SurveyBasket.API/Entities/Question.cs
+++ SurveyBasket.API/Entities/Question.cs
@@ -8,5 +8,6 @@
 		public bool IsActive { get; set; } = true;
 		public Poll Poll { get; set; } = default!;
 		public ICollection<Answer> Answers { get; set; } = [];
+		public ICollection<VoteAnswer> Votes { get; set; } = [];
 	}
 }
