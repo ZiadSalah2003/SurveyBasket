5ce6e249855c460da4b2296be8f1508a81828661
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-19 22:34:59 +0300

    update answer error to contains status code

--- SurveyBasket.API/Errors/AnswerErrors.cs
+++ SurveyBasket.API/Errors/AnswerErrors.cs
@@ -2,6 +2,6 @@
 {
 	public static class AnswerErrors
 	{
-		public static readonly Error PollNotFound = new("Poll.NotFound", "No poll was found with the given id");
+		public static readonly Error PollNotFound = new("Poll.NotFound", "No poll was found with the given id", StatusCodes.Status404NotFound);
 	}
 }
