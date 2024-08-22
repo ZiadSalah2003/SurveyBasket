8674401651c53dd7c6845a4644a7c6b82bd1a7cb
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-22 23:20:16 +0300

    Update user error with new error for registration

--- SurveyBasket.API/Errors/UserErrors.cs
+++ SurveyBasket.API/Errors/UserErrors.cs
@@ -5,8 +5,17 @@ namespace SurveyBasket.API.Errors
 	public static class UserErrors
 	{
 		public static readonly Error InvalidCredentails = new("User.InvalidCredentials", "Invalid Email Or Password", StatusCodes.Status401Unauthorized);
+
 		public static readonly Error InvalidJwtToken = new("User.InvalidJwtToken", "Invalid Jwt token", StatusCodes.Status401Unauthorized);
 
 		public static readonly Error InvalidRefreshToken = new("User.InvalidRefreshToken", "Invalid refresh token", StatusCodes.Status401Unauthorized);
+
+		public static readonly Error DuplicatedEmail = new("User.DuplicatedEmail", "Another user with the same email is already exists", StatusCodes.Status409Conflict);
+
+		public static readonly Error EmailNotConfirmed =new("User.EmailNotConfirmed", "Email is not confirmed", StatusCodes.Status401Unauthorized);
+
+		public static readonly Error InvalidCode =new("User.InvalidCode", "Invalid code", StatusCodes.Status401Unauthorized);
+
+		public static readonly Error DuplicatedConfirmation =new("User.DuplicatedConfirmation", "Email already confirmed", StatusCodes.Status400BadRequest);
 	}
 }
