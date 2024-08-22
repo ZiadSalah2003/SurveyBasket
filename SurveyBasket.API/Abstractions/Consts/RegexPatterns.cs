3378700550acb413b6bea080d1692aab62dab083
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-22 23:19:55 +0300

    add regex for password

---
+++ SurveyBasket.API/Abstractions/Consts/RegexPatterns.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyBasket.API.Abstractions.Consts
+{
+	public static class RegexPatterns
+	{
+		public const string Password = "(?=(.*[0-9]))(?=.*[\\!@#$%^&*()\\\\[\\]{}\\-_+=~`|:;\"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
+
+	}
+}
