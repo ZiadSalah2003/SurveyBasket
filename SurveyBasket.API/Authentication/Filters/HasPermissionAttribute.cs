7288c737971133cb9d93d6faa4a4991d053ac9b2
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-25 20:42:07 +0300

    add configuration of my permission to use it in endpoints

---
+++ SurveyBasket.API/Authentication/Filters/HasPermissionAttribute.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace SurveyBasket.API.Authentication.Filters
+{
+	public class HasPermissionAttribute(string permission) : AuthorizeAttribute(permission)
+	{
+
+	}
+}
