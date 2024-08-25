38380e65d5ba94fc4ad7b7ddf821b5eeeae21e64
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-25 20:38:19 +0300

    seed default data in user table

--- SurveyBasket.API/Persistence/EntitiesConfigrations/UserConfigration.cs
+++ SurveyBasket.API/Persistence/EntitiesConfigrations/UserConfigration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SurveyBasket.API.Abstractions.Consts;
 
 namespace SurveyBasket.API.Persistence.EntitiesConfigrations
 {
@@ -9,9 +10,27 @@ namespace SurveyBasket.API.Persistence.EntitiesConfigrations
 		{
 			builder.OwnsMany(x => x.RefreshTokens).ToTable("RefreshTokens").WithOwner().HasForeignKey("UserId");
 
-			builder.Property(x=>x.FirstName).HasMaxLength(100);
+			builder.Property(x => x.FirstName).HasMaxLength(100);
 			builder.Property(x => x.LastName).HasMaxLength(100);
 
+			//Default Data
+
+			var passwordHasher = new PasswordHasher<ApplicationUser>();
+
+			builder.HasData(new ApplicationUser
+			{
+				Id = DefaultUsers.AdminId,
+				FirstName = "Survay Basket",
+				LastName = "Admin",
+				UserName = DefaultUsers.AdminEmail,
+				NormalizedUserName = DefaultUsers.AdminEmail.ToUpper(),
+				Email = DefaultUsers.AdminEmail,
+				NormalizedEmail = DefaultUsers.AdminEmail.ToUpper(),
+				SecurityStamp = DefaultUsers.AdminSecurityStamp,
+				ConcurrencyStamp = DefaultUsers.AdminConcurrencyStamp,
+				EmailConfirmed = true,
+				PasswordHash= passwordHasher.HashPassword(null!, DefaultUsers.AdminPassword)
+			});
 		}
 	}
 }
