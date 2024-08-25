4308fd9e4e4c8263536733c516d11a92ce58f7a6
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-25 20:39:16 +0300

    seed default data in user role table

---
+++ SurveyBasket.API/Persistence/EntitiesConfigrations/UserRoleConfigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SurveyBasket.API.Abstractions.Consts;
+
+namespace SurveyBasket.API.Persistence.EntitiesConfigrations
+{
+	public class UserRoleConfigration : IEntityTypeConfiguration<IdentityUserRole<string>>
+	{
+		public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+		{
+			//Default Data
+			builder.HasData(new IdentityUserRole<string>
+			{
+				UserId = DefaultUsers.AdminId,
+				RoleId = DefaultRoles.AdminRoleId
+			});
+		}
+	}
+}
