c2980a316dba5565aadfc7085e970e1d5914f163
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-25 20:38:41 +0300

    seed default data in roles table

---
+++ SurveyBasket.API/Persistence/EntitiesConfigrations/RoleConfigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SurveyBasket.API.Abstractions.Consts;
+
+namespace SurveyBasket.API.Persistence.EntitiesConfigrations
+{
+	public class RoleConfigration : IEntityTypeConfiguration<ApplicationRole>
+	{
+		public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+		{
+			//Default Data
+			builder.HasData([
+				new ApplicationRole
+				{
+					Id = DefaultRoles.AdminRoleId,
+					Name = DefaultRoles.Admin,
+					NormalizedName = DefaultRoles.Admin.ToUpper(),
+					ConcurrencyStamp = DefaultRoles.AdminRoleConcurrencyStamp
+				},
+				new ApplicationRole
+				{
+					Id = DefaultRoles.MemberRoleId,
+					Name = DefaultRoles.Member,
+					NormalizedName = DefaultRoles.Member.ToUpper(),
+					ConcurrencyStamp = DefaultRoles.MemberRoleConcurrencyStamp,
+					IsDefault = true
+				}
+			]);
+
+		}
+	}
+}
