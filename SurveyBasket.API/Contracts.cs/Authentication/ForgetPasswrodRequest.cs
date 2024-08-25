bdeb4d19da5a0974adf2a9adf6146ecec033b15e
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-25 03:28:58 +0300

    add Forget password request and validator

---
+++ SurveyBasket.API/Contracts.cs/Authentication/ForgetPasswrodRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace SurveyBasket.API.Contracts.cs.Authentication
+{
+	public record ForgetPasswrodRequest(
+		string Email	
+	);
+}
---
+++ SurveyBasket.API/Contracts.cs/Authentication/ForgetPasswrodRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace SurveyBasket.API.Contracts.cs.Authentication
+{
+	public class ForgetPasswrodRequestValidator : AbstractValidator<ForgetPasswrodRequest>
+	{
+		public ForgetPasswrodRequestValidator()
+		{
+			RuleFor(x => x.Email)
+				.NotEmpty()
+				.EmailAddress();
+		}
+	}
+}
