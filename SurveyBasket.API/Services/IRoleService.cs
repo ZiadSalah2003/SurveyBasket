d7cce0f2caa380c5f1a12783d26395fe2a9f32cb
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-28 19:30:18 +0300

    add role endpoins and there implementation

---
+++ SurveyBasket.API/Services/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using SurveyBasket.API.Contracts.cs.Roles;
+
+namespace SurveyBasket.API.Services
+{
+	public interface IRoleService
+	{
+		Task<IEnumerable<RoleResponse>> GetAllAsync(bool? includeDisabled = false, CancellationToken cancellationToken = default);
+		Task<Result<RoleDetailResponse>> GetAsync(string id);
+		Task<Result<RoleDetailResponse>> AddAsync(RoleRequest request);
+		Task<Result> UpdateAsync(string id, RoleRequest request);
+		Task<Result> ToggleStatusAsync(string id);
+	}
+}
