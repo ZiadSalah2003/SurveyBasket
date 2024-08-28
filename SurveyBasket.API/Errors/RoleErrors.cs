8b26ec8f095433824ea9d233bf9f6086656da684
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-28 19:31:13 +0300

    add role error for result response

---
+++ SurveyBasket.API/Errors/RoleErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace SurveyBasket.API.Errors
+{
+	public static class RoleErrors
+	{
+		public static readonly Error RoleNotFound = new("Role.NotFound", "Role is not found", StatusCodes.Status404NotFound);
+
+		public static readonly Error InvalidPermissions = new("rOLE.InvalidPermissions", "Invalid Permissions", StatusCodes.Status400BadRequest);
+
+		public static readonly Error DuplicatedRole = new("Role.DuplicatedRole", "Another role with the same name is already exists", StatusCodes.Status409Conflict);
+	}
+}
