9df8d9a69972bc7370264ba1c1c67f752a0e281c
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-31 19:44:09 +0300

    add Create user request and there validation

---
+++ SurveyBasket.API/Contracts.cs/Users/CreateUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SurveyBasket.API.Contracts.cs.Users
+{
+	public record CreateUserRequest(
+		string FirstName,
+		string LastName,
+		string Email,
+		string Password,
+		IList<string> Roles
+	);
+}
---
+++ SurveyBasket.API/Contracts.cs/Users/CreateUserRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+
+namespace SurveyBasket.API.Contracts.cs.Users
+{
+	public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+	{
+		public CreateUserRequestValidator()
+		{
+			RuleFor(x => x.Email)
+				.NotEmpty()
+				.EmailAddress();
+
+			RuleFor(x => x.Password)
+				.NotEmpty()
+				.Matches(RegexPatterns.Password)
+				.WithMessage("Passwrod should be at least 8 digits and should contains lower case, nonalphanumeric and uppercase");
+
+			RuleFor(x => x.FirstName)
+				.NotEmpty()
+				.Length(3, 100);
+
+			RuleFor(x => x.LastName)
+				.NotEmpty()
+				.Length(3, 100);
+
+			RuleFor(x => x.Roles)
+			   .NotNull()
+			   .NotEmpty();
+
+			RuleFor(x => x.Roles)
+				.Must(x => x.Distinct().Count() == x.Count)
+				.WithMessage("You can not duplicate Role for the same user")
+				.When(x => x.Roles != null);
+
+
+		}
+	}
+}
