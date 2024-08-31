afc8e064df07734bbeaffac3dd2bcafb8655cedb
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-31 19:44:22 +0300

    add Update user request and there validation

---
+++ SurveyBasket.API/Contracts.cs/Users/UpdateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SurveyBasket.API.Contracts.cs.Users
+{
+	public record UpdateUserRequest(
+		string FirstName,
+		string LastName,
+		string Email,
+		IList<string> Roles
+	);
+}
---
+++ SurveyBasket.API/Contracts.cs/Users/UpdateUserRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace SurveyBasket.API.Contracts.cs.Users
+{
+	public class UpdateUserRequestValidator : AbstractValidator<UpdateUserRequest>
+	{
+		public UpdateUserRequestValidator()
+		{
+			RuleFor(x => x.Email)
+				.NotEmpty()
+				.EmailAddress();
+
+			RuleFor(x => x.FirstName)
+				.NotEmpty()
+				.Length(3, 100);
+
+			RuleFor(x => x.LastName)
+				.NotEmpty()
+				.Length(3, 100);
+
+			RuleFor(x => x.Roles)
+			   .NotNull()
+			   .NotEmpty();
+
+			RuleFor(x => x.Roles)
+				.Must(x => x.Distinct().Count() == x.Count)
+				.WithMessage("You can not duplicate Role for the same user")
+				.When(x => x.Roles != null);
+		}
+	}
+}
