3522f7b75577f55446b7d49d713a6a34ddbbd3df
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-08-31 19:48:18 +0300

    update users and roles error

--- SurveyBasket.API/Errors/RoleErrors.cs
+++ SurveyBasket.API/Errors/RoleErrors.cs
@@ -4,7 +4,7 @@
 	{
 		public static readonly Error RoleNotFound = new("Role.NotFound", "Role is not found", StatusCodes.Status404NotFound);
 
-		public static readonly Error InvalidPermissions = new("rOLE.InvalidPermissions", "Invalid Permissions", StatusCodes.Status400BadRequest);
+		public static readonly Error InvalidPermissions = new("Role.InvalidPermissions", "Invalid Permissions", StatusCodes.Status400BadRequest);
 
 		public static readonly Error DuplicatedRole = new("Role.DuplicatedRole", "Another role with the same name is already exists", StatusCodes.Status409Conflict);
 	}
--- SurveyBasket.API/Errors/UserErrors.cs
+++ SurveyBasket.API/Errors/UserErrors.cs
@@ -17,5 +17,11 @@ namespace SurveyBasket.API.Errors
 		public static readonly Error InvalidCode =new("User.InvalidCode", "Invalid code", StatusCodes.Status401Unauthorized);
 
 		public static readonly Error DuplicatedConfirmation =new("User.DuplicatedConfirmation", "Email already confirmed", StatusCodes.Status400BadRequest);
+
+		public static readonly Error DisabledUser = new("User.DisabledUser", "Disabled User please contact your adminstrator", StatusCodes.Status401Unauthorized);
+		
+		public static readonly Error UserNotFound = new("User.UserNotFound", "User is not found", StatusCodes.Status404NotFound);
+
+		public static readonly Error InvalidRoles = new("User.InvalidRoles", "Invalid Roles", StatusCodes.Status400BadRequest);
 	}
 }
