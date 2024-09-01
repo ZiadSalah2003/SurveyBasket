e2fa6f2e67483bd8e5dee6015f4d80e1eb8f3c70
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-01 03:19:32 +0300

    add paginatd class

---
+++ SurveyBasket.API/Abstractions/PaginatedList.cs
@@ -0,0 +1,24 @@
+﻿namespace SurveyBasket.API.Abstractions
+{
+	public class PaginatedList<T>
+	{
+		public List<T> Items { get; private set; } 
+		public int PageNumber { get; private set; } 
+		public int TotalPages { get; private set; }
+		public bool HasPreviosPage => PageNumber > 1;
+		public bool HasNextPage => PageNumber < TotalPages;
+
+        public PaginatedList(List<T> items, int pageNumber,int count,int pageSize)
+        {
+            Items = items;
+			PageNumber = pageNumber;
+			TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+		public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize,CancellationToken cancellationToken = default)
+		{
+			var count = await source.CountAsync(cancellationToken);
+			var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken);
+			return new PaginatedList<T>(items, pageNumber, count, pageSize);
+		}
+    }
+}
