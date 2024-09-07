175cd103e63203fbef83b781ce798efa77bb3f54
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-08 01:25:59 +0300

    add rate limit const string

---
+++ SurveyBasket.API/Abstractions/Consts/RateLimiters.cs
@@ -0,0 +1,12 @@
+﻿namespace SurveyBasket.API.Abstractions.Consts
+{
+	public static class RateLimiters
+	{
+		public const string IpLimiter = "ipLimit";
+		public const string UserLimiter = "userLimit";
+		public const string Concurrency = "concurrency";
+		public const string Token = "token";
+		public const string Fixed = "fixed";
+		public const string Sliding = "sliding";
+	}
+}
