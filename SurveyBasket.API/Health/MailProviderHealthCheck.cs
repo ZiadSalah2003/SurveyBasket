d6be51f0af91e44786348b116b078b5ca0b8e36f
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-09-07 19:52:04 +0300

    add custom health check for mail service

---
+++ SurveyBasket.API/Health/MailProviderHealthCheck.cs
@@ -0,0 +1,29 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using SurveyBasket.API.Settings;
+
+namespace SurveyBasket.API.Health
+{
+	public class MailProviderHealthCheck(IOptions<MailSettings> maillSettins) : IHealthCheck
+	{
+		private readonly MailSettings _maillSettins = maillSettins.Value;
+		public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+		{
+			try
+			{
+				using var smtp = new SmtpClient();
+
+				smtp.Connect(_maillSettins.Host, _maillSettins.Port, SecureSocketOptions.StartTls, cancellationToken);
+				smtp.Authenticate(_maillSettins.Mail, _maillSettins.Password, cancellationToken);
+			
+				return await Task.FromResult(HealthCheckResult.Healthy());
+			}
+			catch(Exception exception)
+			{
+				return await Task.FromResult(HealthCheckResult.Unhealthy(exception: exception));
+			}
+		}
+	}
+}
